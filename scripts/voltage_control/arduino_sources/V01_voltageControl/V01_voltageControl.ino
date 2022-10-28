26c1e99ce1d7715a042782f59680051e160a2916
Erik Plesko <erik.plesko@gmail.com>
2022-10-28 11:59:29 +0200

    Arduino two tips integration

--- scripts/voltage_control/arduino_sources/V01_voltageControl/V01_voltageControl.ino
+++ scripts/voltage_control/arduino_sources/V01_voltageControl/V01_voltageControl.ino
@@ -9,8 +9,25 @@
  * Voltage units: mV
  * Time units: ms
  *
+ * Serial commands description:
+ * 		! - set
+ * 		A - Answer that a command has been executed
+ *		? - get
+ * 		# - Answer to get
+ * 		
+ * 		Arguments are separated with spaces
+ * 		
+ * 		Example 1: !V 1 100 -> set voltage (tip 1) (to 100 mV) -> AV 1 100
+ * 		Example 2: ?V 2 -> get voltage (tip 2) -> #V 2 1872
+ * 
+ * 		For simple overview of functions and arguments read the comments
+ * 		of lines where functions are added to scmd (scmd.addCommand()... )
+ * 
  *
- *
+ * V1.2 - 28.10.2022 by Erik:
+ * - Added support for two tips
+ * 		setting and getting the voltage of the tips (one by one)
+ * 		made the timeouts work separately for each tip - just the tip you don't specify the voltage will shut off after the timeout (if timeout is on)
  *
  *
  *
@@ -27,17 +44,21 @@
 #include <SerialCommand.h>
 
 
-// pins 5, 6 with frequency 980 Hz
-#define VinPin 5        // Output voltage, but labeled as on the current generator: Vin
-#define VLEDPin 6       // LED, used to visualise Vin value
-#define VsensePin A0
+// pins 5, 6 with frequency 980 Hz (all PWM pins: 3, 5, 6, 9, 10, 11, 490 Hz)
+#define VinPin1 5        // Output voltage for the tip 1, but labeled as on the current generator: Vin
+#define VinPin2 6        // Output voltage for the tip 2, but labeled as on the current generator: Vin
+#define VLEDPin1 9       // LED, used to visualise Vin1 value
+#define VLEDPin2 10       // LED, used to visualise Vin2 value
+#define VsensePin1 A0    // V-sense1 pin
+#define VsensePin2 A1    // V-sense2 pin
 
 
 bool autoTimeoutOn = true;              // Output voltage is set to zero if there are no commands received for time of autoTimeoutTime
 unsigned int autoTimeoutTime = 200;     // milliseconds
-unsigned long autoTimeoutTimer = 0;     // Timer. it is reset every time voltage command is received
+unsigned long autoTimeoutTimer1 = 0;     // Timer for tip 1. it is reset every time voltage command is received for that tip
+unsigned long autoTimeoutTimer2 = 0;     // Timer for tip 2. it is reset every time voltage command is received for that tip
 
-int Vmax = 2000;    // Max voltage to be set on the output pin VinPin
+int Vmax = 2000;    // Max voltage to be set on any of the output pins VinPin
 
 // Serial Command:
 SerialCommand scmd;
@@ -52,11 +73,12 @@ bool printAll = true;
 void setup() {
 	Serial.begin(19200);
 
-	scmd.addCommand("!VI", cmd_set_voltage);            // _ #voltageIn [mV]   // Set voltage In (it goes out, but the signal goes to V-in on the current generator)
-	scmd.addCommand("?VS", cmd_get_voltage);            // _ /                 // Read the voltage (connected to V-sense)
+	scmd.addCommand("!V", cmd_set_voltage);            // _ #tip_number [] #voltageIn [mV]   // Set voltage In for a specific tip (it goes out, but the signal goes to V-in on the current generator)
+	scmd.addCommand("?V", cmd_get_voltage);            // _ #tip_number []                  // Read the voltage of a specific pin (connected to V-sense)
+
 	scmd.addCommand("!TT", cmd_set_auto_timeout_time);  // _ #autoTimeoutTime
 	scmd.addCommand("!TO", cmd_set_auto_timeout_on);    // _ #autoTimeoutOn (0 or 1)
-    scmd.addCommand("!VM", cmd_set_voltage_max);        // _ #Vmax (in range 0 to 3000)
+	scmd.addCommand("!VM", cmd_set_voltage_max);        // _ #Vmax (in range 0 to 3000)
 
 	// General:
 	scmd.addCommand("!PA", cmd_setPrintAll);            // _ #printAll (0 or 1)
@@ -68,7 +90,7 @@ void setup() {
 	pinMode(LED_BUILTIN, OUTPUT);   // Builtin LED
 
 	Serial.println(F("Setup done."));
-	Serial.println(F("File: V01_voltageControl.1"));
+	Serial.println(F("File: V01_voltageControl.2"));
 
 }
 
@@ -78,9 +100,13 @@ void loop() {
 	scmd.readSerial();
 
 	if (autoTimeoutOn) {
-		if (millis() > autoTimeoutTimer + autoTimeoutTime) {
-			set_voltage_pin(0);
+		if (millis() > autoTimeoutTimer1 + autoTimeoutTime) {
+			set_voltage_pin(1, 0);
 		}
+		if (millis() > autoTimeoutTimer2 + autoTimeoutTime) {
+			set_voltage_pin(2, 0);
+		}
+
 	}
 	
 
@@ -91,10 +117,10 @@ void loop() {
 	// delay(10);
 	// set_voltage_pin(100);
 	// delay(10);
-    
-    
-    
-    
+	
+	
+	
+	
 }
 
 
@@ -124,22 +150,25 @@ long AD_2_mV(long AD) {
 }
 
 
-void set_voltage_pin(int voltage) {
+void set_voltage_pin(int tip_number, int voltage) {
 	byte DA;    // value to set to DAC
 
-	// TODO:
-	// Do it nicer: Limit the output voltage yo Vmax (3V - or whatever)
-    if (voltage > Vmax) {
-        voltage = Vmax;
-    }
-
+	if (voltage > Vmax) {
+		voltage = Vmax;
+	}
 
 	DA = mV_2_DA(voltage);
 
-	analogWrite(VinPin, DA);    
-	analogWrite(VLEDPin, DA);
+	if (tip_number == 1) {
+		analogWrite(VinPin1, DA);    
+		analogWrite(VLEDPin1, DA);
+		autoTimeoutTimer1 = millis();   
+	} else if (tip_number == 2) {
+		analogWrite(VinPin2, DA);    
+		analogWrite(VLEDPin2, DA);
+		autoTimeoutTimer2 = millis();   
+	}    
 
-	autoTimeoutTimer = millis();   
 }
 
 
@@ -186,36 +215,72 @@ void cmd_test() {
 
 
 void cmd_set_voltage() {
+	// Set the voltage based on the serial message value.
+	// First serial argument is tip number and the second gives the voltage value
 	char * arg;
 	int val;
 
 	arg = scmd.next();
 	if ( arg != NULL) {
-		val = atol( arg );
+		int tip_number = atol( arg );
 
-		set_voltage_pin(val);
+		arg = scmd.next();
+		if ( arg != NULL) {
+			val = atol( arg );
 
-    Serial.print(F("#VI ")); Serial.println(val);
-		
-		if (printAll) {
-			Serial.print(F("voltage set to "));
-			Serial.println(val);
-		}
-	}
+			if (tip_number == 1 || tip_number == 2) {
+				set_voltage_pin(tip_number, val);
 
+				Serial.print(F("AV ")); 
+				Serial.print(tip_number); 
+				Serial.print(F(" ")); 
+				Serial.println(val);
+			} else {
+				Serial.print(F("ERR - unsupported tip number: ")); Serial.println(tip_number);
+				return;
+			}
 
+			if (printAll) {
+				Serial.print(F("voltage set to "));
+				Serial.println(val);
+			}
+		} else Serial.println(F("ERR - cmd_set_voltage(): Not enough arguments."));
+	} else Serial.println(F("ERR - cmd_set_voltage(): Not enough arguments."));
 }
+
 
 void cmd_get_voltage () {
-	// TODO:
+	// Get the voltage Of the specified pin and send it over serial.
+	// The serial argument in the command calling the function specifies tip number .
+	char * arg;
 
+	arg = scmd.next();
+	if ( arg != NULL) {
+		int tip_number = atol( arg );
+		int AD_val;
+		if (tip_number == 1) {
+			AD_val = analogRead(VsensePin1);
+		} else if (tip_number == 2) {
+			AD_val = analogRead(VsensePin2);
+		} else {
+			Serial.print(F("ERR - unsupported tip number: ")); Serial.println(tip_number);
+			return;
+		}
 
-  Serial.print(F("#VS ")); Serial.println(AD_2_mV((analogRead(VsensePin))));
-	if (printAll) {
-		Serial.println(F("That was VsensePin [mV] "));
-	}
+		Serial.print(F("#V ")); 
+		Serial.print(tip_number); 
+		Serial.print(F(" ")); 
+		Serial.println(AD_2_mV(AD_val));
 	
+		if (printAll) {
+			Serial.println(F("That was VsensePin [mV] "));
+		}
+	} else {
+		Serial.println(F("ERR - cmd_get_voltage(): Not enough arguments."));
+
+	}
 }
+
 
 void cmd_set_auto_timeout_time () {
 	char * arg;
@@ -254,14 +319,14 @@ void cmd_set_voltage_max () {
 	arg = scmd.next();
 	if ( arg != NULL) {
 		val = atol( arg );
-        // Vmax should be in the range [0, 3000]
-        if (val >= 0 && val <= 3000) {
-		    Vmax = val;
-        } else {
-            if (printAll) {
-                Serial.println(F("Value for Vmax is out of range [0, 3000]."));
-            }
-        }
+		// Vmax should be in the range [0, 3000]
+		if (val >= 0 && val <= 3000) {
+			Vmax = val;
+		} else {
+			if (printAll) {
+				Serial.println(F("Value for Vmax is out of range [0, 3000]."));
+			}
+		}
 		if (printAll) {
 			Serial.print(F("Vmax set to "));
 			Serial.println(Vmax);
@@ -285,54 +350,5 @@ void cmd_setPrintAll() {
 }
 
 
-
 // #### End of document #################################################################
 // ######################################################################################
-
-
-
-
-
-
-
-
-
-// void cmd_moveJ() {
-// 	char * arg;
-// 	long steps1;
-// 	long steps2;
-// 	int servoValOld;
-
-// 	arg = scmd.next();
-// 	if ( arg != NULL) {
-		
-// 		steps1 = atol( arg );
-
-// 		arg = scmd.next();
-// 		if (arg != NULL) {
-			
-// 			steps2 = atol( arg );
-
-// 			stepper1.moveTo(steps1);
-// 			stepper2.moveTo(steps2);
-
-// 			// Move servo:
-// 			arg = scmd.next();
-// 			if (arg != NULL) {
-// 				servoValOld = servo3.read();
-// 				servo3.write(atol( arg ));
-// 				delay(2*abs(servoValOld-servo3.read()));
-// 			}
-
-// 			targetReached = false;
-// 			runMotorsBySpeed = false;
-
-// 			if (printAll) {
-// 				Serial.print("steps1: "); Serial.println(steps1);
-// 				Serial.print("steps2: "); Serial.println(steps2);
-// 			}
-// 		}
-		
-// 	}
-
-// }
