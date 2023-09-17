cb81f7d5734dd922b3dfbf54ddbcf7c731bddf35
Erik Plesko <erik.plesko@gmail.com>
2023-09-17 16:11:57 +0200

    Changed serial API and bumped to v0.2.0.

--- magnetic_tweezers_brugueslab/scripts/voltage_control/arduino_sources/V02_pcb_hotfix/V02_pcb_hotfix.ino
+++ magnetic_tweezers_brugueslab/scripts/voltage_control/arduino_sources/V02_pcb_hotfix/V02_pcb_hotfix.ino
@@ -98,10 +98,10 @@ void setup() {
     // scmd.addCommand("!ST", cmd_set_single_tip)                  //_ #tipNumber #voltageIn
     // Using the command to set 500 mV for tip number 0 looks like: !ST 0 500
 
-    scmd.addCommand("!ST", cmd_set_single_tip);         // _ #tipNumber #voltageIn      // Set voltage In for a single tip (tip number starts with zero)
-	scmd.addCommand("!AT", cmd_set_all_tips);           // _ #voltageInTip0 #voltageInTip1  // Set voltage for all tips
-    scmd.addCommand("?ST", cmd_get_single_tip);         // _ #tipNumber                 // Read voltage for a single tip (tip number starts with zero)
-	scmd.addCommand("?AT", cmd_get_all_tips);           // _ /                          // Read voltage of all tips
+    scmd.addCommand("!SI", cmd_set_single_v_in);        // _ #tipNumber #voltageIn      // Set voltage In for a single tip (tip number starts with zero)
+	scmd.addCommand("!AI", cmd_set_all_v_in);           // _ #voltageInTip0 #voltageInTip1  // Set voltage In for all tips
+    scmd.addCommand("?SS", cmd_get_single_v_sense);     // _ #tipNumber                 // Read Vsense for a single tip (tip number starts with zero)
+	scmd.addCommand("?AS", cmd_get_all_v_sense);        // _ /                          // Read Vsense of all tips
 	
     scmd.addCommand("!TT", cmd_set_auto_timeout_time);  // _ #autoTimeoutTime           // Set the time for auto time out (turn off) of the voltage of tips
 	scmd.addCommand("!TO", cmd_set_auto_timeout_on);    // _ #autoTimeoutOn (0 or 1)    // Toggle functionality
@@ -129,7 +129,7 @@ void setup() {
     mcp4728.setChannelValue(MCP4728_CHANNEL_D, 0);
 
 	Serial.println(F("Setup done."));
-	Serial.println(F("File: V02_pcb_hotfix.ino, v0.1.2"));
+	Serial.println(F("File: V02_pcb_hotfix.ino, v0.2.0"));
     Serial.print(F("MAXSERIALCOMMANDS should be 15. It is: ")); Serial.println(MAXSERIALCOMMANDS);
     Serial.println(F("For help type 'help'."));
 	turnOff();
@@ -268,8 +268,8 @@ void cmd_test() {
 	Serial.println(F("End of test function."));
 }
 
-void cmd_set_single_tip() {
-    // Example call: !ST 0 500
+void cmd_set_single_v_in() {
+    // Example call: !SI 0 500
     char * arg;
 	int tip_number;
 	int voltage;
@@ -284,7 +284,7 @@ void cmd_set_single_tip() {
 
 		    set_voltage_pin(tip_number, voltage);
 
-            Serial.print(F("#ST ")); Serial.print(tip_number); Serial.print(F(" ")); Serial.println(voltage);
+            Serial.print(F("#SI ")); Serial.print(tip_number); Serial.print(F(" ")); Serial.println(voltage);
             if (printAll) {
                 Serial.print(F("That was Vin set for tip "));
                 Serial.println(tip_number);
@@ -293,8 +293,8 @@ void cmd_set_single_tip() {
 	}
 }
 
-void cmd_set_all_tips() {
-    // Example call: !AT 150 400
+void cmd_set_all_v_in() {
+    // Example call: !AI 150 400
 
     char * arg;
 	int voltage_0;
@@ -305,19 +305,18 @@ void cmd_set_all_tips() {
 		voltage_0 = atol( arg );
         set_voltage_pin(0, voltage_0);
 
-
         arg = scmd.next();
         if (arg != NULL) {
             voltage_1 = atol( arg );
             set_voltage_pin(1, voltage_1);
         }
 
-        Serial.print(F("#AT ")); Serial.print(voltage_0); Serial.print(F(" ")); Serial.println(voltage_1);
+        Serial.print(F("#AI ")); Serial.print(voltage_0); Serial.print(F(" ")); Serial.println(voltage_1);
     }
 }
 
-void cmd_get_single_tip() {
-    // Example call: ?ST 0
+void cmd_get_single_v_sense() {
+    // Example call: ?SS 0
     char * arg;
 	int tip_number;
 
@@ -325,7 +324,7 @@ void cmd_get_single_tip() {
 	if (arg != NULL && is_valid_tip_number(tip_number)) {
 		tip_number = atol( arg );
 
-        Serial.print(F("#ST ")); Serial.println(get_voltage_pin(tip_number));
+        Serial.print(F("#SS ")); Serial.println(get_voltage_pin(tip_number));
         if (printAll) {
             Serial.print(F("That was Vsense read for tip "));
             Serial.println(tip_number);
@@ -333,7 +332,7 @@ void cmd_get_single_tip() {
     }
 }
 
-void cmd_get_all_tips() {
+void cmd_get_all_v_sense() {
     // Example call: ?AT
 	long voltage_0;
 	long voltage_1;
@@ -341,7 +340,7 @@ void cmd_get_all_tips() {
     voltage_0 = get_voltage_pin(0);
     voltage_1 = get_voltage_pin(1);
             
-    Serial.print(F("#AT ")); Serial.print(voltage_0); Serial.print(F(" ")); Serial.println(voltage_1);
+    Serial.print(F("#AS ")); Serial.print(voltage_0); Serial.print(F(" ")); Serial.println(voltage_1);
 }
 
 // -------------------------------------------------------------------------------------
@@ -443,25 +442,31 @@ void cmd_print_state() {
 }
 
 void cmd_print_help() {
-    Serial.println(F("----------- Help -------------------"));
-    Serial.println(F("// Example for using and reading this documentation: "));
-    Serial.println(F("// Command is described in the line declaring the command and the comment. _ in the comment denotes command string (!ST in this case):"));
-    Serial.println(F("// scmd.addCommand('!ST', cmd_set_single_tip)                  //_ #tipNumber #voltageIn"));
-    Serial.println(F("// Using the command to set 500 mV for tip number 0 looks like: !ST 0 500"));
-    Serial.println(F("scmd.addCommand('!ST', cmd_set_single_tip);         // _ #tipNumber #voltageIn      // Set voltage In for a single tip (tip number starts with zero)"));
-    Serial.println(F("scmd.addCommand('!AT', cmd_set_all_tips);           // _ #voltageInTip0 #voltageInTip1  // Set voltage for all tips"));
-    Serial.println(F("scmd.addCommand('?ST', cmd_get_single_tip);         // _ #tipNumber                 // Read voltage for a single tip (tip number starts with zero)"));
-    Serial.println(F("scmd.addCommand('?AT', cmd_get_all_tips);           // _ /                          // Read voltage of all tips"));
-    Serial.println(F("	"));
-    Serial.println(F("scmd.addCommand('!TT', cmd_set_auto_timeout_time);  // _ #autoTimeoutTime           // Set the time for auto time out (turn off) of the voltage of tips"));
-    Serial.println(F("scmd.addCommand('!TO', cmd_set_auto_timeout_on);    // _ #autoTimeoutOn (0 or 1)    // Toggle functionality"));
-    Serial.println(F("scmd.addCommand('!VM', cmd_set_voltage_max);        // _ #Vmax (in range 0 to 3000) // Set maximum allowed voltage for tips (3000 is a hard limit in the source file)"));
-    Serial.println(F("scmd.addCommand('!LT', cmd_set_update_leds_time);   // _ #update_vsense_leds_time   // Set time period of Vsense LEDs updating based on read voltage"));
-    Serial.println(F("// General:"));
-    Serial.println(F("scmd.addCommand('!PS', cmd_print_state);            // _ /                          // Print state of the configuration"));
-    Serial.println(F("scmd.addCommand('!PA', cmd_setPrintAll);            // _ #printAll (0 or 1)         // Toggle verbosity of printouts"));
-    Serial.println(F("scmd.addCommand('help', cmd_print_help);            // _ /                          // Print (this) help"));
-    Serial.println(F("scmd.addCommand('test', cmd_test);                  // _ (#a #b #c #d ...)          // Test function that echoes the arguments and blinks the LEDs on the PCB"));
-    Serial.println(F("scmd.addDefaultHandler(unrecognized);               // Gets executed when the command was not recognized (Note the comment about MAXSERIALCOMMANDS in the code above)"));
-    Serial.println(F("----------- End of help ------------"));
+    // When updating the help make sure to update the version here to match the source file version.
+    // v0.X.0 - the X should change when the serial API changes (so when this help needs to be changed)
+    Serial.println(F("----------- Help - for version v0.2.0 ----------------"));
+
+    Serial.println(F("    // Example for using and reading this documentation: "));
+    Serial.println(F("    // Command is described in the line declaring the command and the comment. _ in the comment denotes command string (!ST in this case):"));
+    Serial.println(F("    // scmd.addCommand('!ST', cmd_set_single_tip)                  //_ #tipNumber #voltageIn"));
+    Serial.println(F("    // Using the command to set 500 mV for tip number 0 looks like: !ST 0 500"));
+    Serial.println(F(""));
+    Serial.println(F("    scmd.addCommand('!SI', cmd_set_single_v_in);        // _ #tipNumber #voltageIn      // Set voltage In for a single tip (tip number starts with zero)"));
+    Serial.println(F("    scmd.addCommand('!AI', cmd_set_all_v_in);           // _ #voltageInTip0 #voltageInTip1  // Set voltage In for all tips"));
+    Serial.println(F("    scmd.addCommand('?SS', cmd_get_single_v_sense);     // _ #tipNumber                 // Read Vsense for a single tip (tip number starts with zero)"));
+    Serial.println(F("    scmd.addCommand('?AS', cmd_get_all_v_sense);        // _ /                          // Read Vsense of all tips"));
+    Serial.println(F("    "));
+    Serial.println(F("    scmd.addCommand('!TT', cmd_set_auto_timeout_time);  // _ #autoTimeoutTime           // Set the time for auto time out (turn off) of the voltage of tips"));
+    Serial.println(F("    scmd.addCommand('!TO', cmd_set_auto_timeout_on);    // _ #autoTimeoutOn (0 or 1)    // Toggle functionality"));
+    Serial.println(F("    scmd.addCommand('!VM', cmd_set_voltage_max);        // _ #Vmax (in range 0 to 3000) // Set maximum allowed voltage for tips (3000 is a hard limit in the source file)"));
+    Serial.println(F("    scmd.addCommand('!LT', cmd_set_update_leds_time);   // _ #update_vsense_leds_time   // Set time period of Vsense LEDs updating based on read voltage"));
+    Serial.println(F(""));
+    Serial.println(F("    // General:"));
+    Serial.println(F("    scmd.addCommand('!PS', cmd_print_state);            // _ /                          // Print state of the configuration"));
+    Serial.println(F("    scmd.addCommand('!PA', cmd_setPrintAll);            // _ #printAll (0 or 1)         // Toggle verbosity of printouts"));
+    Serial.println(F("    scmd.addCommand('help', cmd_print_help);            // _ /                          // Print (this) help"));
+    Serial.println(F("    scmd.addCommand('test', cmd_test);                  // _ (#a #b #c #d ...)          // Test function that echoes the arguments and blinks the LEDs on the PCB"));
+    Serial.println(F("    scmd.addDefaultHandler(unrecognized);               // Gets executed when the command was not recognized (Note the comment about MAXSERIALCOMMANDS in the code above)"));
+
+    Serial.println(F("----------- End of help ------------------------------"));
 }
