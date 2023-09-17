357640ea323fcea07dff3b9585f644d453b20b88
Erik Plesko <erik.plesko@gmail.com>
2023-09-17 13:03:00 +0200

    2 Tips working with leds updating.
    still some old stuff in. And it seems only 10 commands can be registered with scmd.

--- magnetic_tweezers_brugueslab/scripts/voltage_control/arduino_sources/V02_pcb_hotfix/V02_pcb_hotfix.ino
+++ magnetic_tweezers_brugueslab/scripts/voltage_control/arduino_sources/V02_pcb_hotfix/V02_pcb_hotfix.ino
@@ -42,8 +42,8 @@
 // LEDs numbers:
 #define T1_VIN_LED_NUMBER 0
 #define T1_VS_LED_NUMBER 1
-#define T1_VIN_LED_NUMBER 2
-#define T1_VS_LED_NUMBER 3
+#define T2_VIN_LED_NUMBER 2
+#define T2_VS_LED_NUMBER 3
 
 // TODO: legacy, from before the pcb board:
 #define LED_PIN     2  // Pin for LEDs on the PCB
@@ -56,11 +56,20 @@
 
 // -- Prepare --------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------
+// Making tips variables in indexable form:
+MCP4728_channel_t vin_pin_of_tip[] = {T1_VIN_PIN_NUMBER, T2_VIN_PIN_NUMBER};
+int vs_pin_of_tip[] = {T1_VS_PIN_NUMBER, T2_VS_PIN_NUMBER};
 
+int vin_led_of_tip[] = {T1_VIN_LED_NUMBER, T2_VIN_LED_NUMBER};
+int vs_led_of_tip[] = {T1_VS_LED_NUMBER, T2_VS_LED_NUMBER};
+
+// Timers:
 bool autoTimeoutOn = true;              // Output voltage is set to zero if there are no commands received for time of autoTimeoutTime
 unsigned int autoTimeoutTime = 200;     // milliseconds
-unsigned long autoTimeoutTimer_1 = 0;     // Timer for tip 1. it is reset every time voltage command is received for tip 1
-unsigned long autoTimeoutTimer_2 = 0;     // Timer for tip 2. it is reset every time voltage command is received for tip 2
+unsigned long auto_time_out_timers[] = {0, 0};     // Timers for tips. They are reset every time voltage is set for each pin respectively.
+
+unsigned int update_vsense_leds_time = 137;         // Time period of updating the Vsense LEDs (by reading Vsense)
+unsigned long update_vsense_leds_timers[] = {0, 0}; // Timers for each tip respectively
 
 int Vmax = 2000;    // Max voltage to be set on the output pin VinPin
 
@@ -85,13 +94,25 @@ void setup() {
 
 	Serial.begin(19200);
 
-	scmd.addCommand("!VI", cmd_set_voltage);            // _ #voltageIn [mV]   // Set voltage In (it goes out, but the signal goes to V-in on the current generator)
-	scmd.addCommand("?VS", cmd_get_voltage);            // _ /                 // Read the voltage (connected to V-sense)
-	scmd.addCommand("!TT", cmd_set_auto_timeout_time);  // _ #autoTimeoutTime
+	// TODO: legacy:
+    // scmd.addCommand("!VI", cmd_set_voltage);            // _ #voltageIn [mV]   // Set voltage In (it goes out, but the signal goes to V-in on the current generator)
+	
+    scmd.addCommand("!ST", cmd_set_single_tip);         // _ #tipNumber #voltageIn [mV]   // Set voltage In for a single tip (tip number starts with zero)
+	scmd.addCommand("!AT", cmd_set_all_tips);           // _ #voltageInTip0 #voltageInTip1 [mV]   // Set voltage In for a single tip
+	
+    // TODO: legacy:
+    // scmd.addCommand("?VS", cmd_get_voltage);            // _ /                  // Read the voltage (connected to V-sense)
+	
+    scmd.addCommand("?ST", cmd_get_single_tip);         // _ #tipNumber         // Read voltage for a single tip (tip number starts with zero)
+	scmd.addCommand("?AT", cmd_get_all_tips);           // _ /                  // Read voltage of all tips
+	
+    scmd.addCommand("!TT", cmd_set_auto_timeout_time);  // _ #autoTimeoutTime
 	scmd.addCommand("!TO", cmd_set_auto_timeout_on);    // _ #autoTimeoutOn (0 or 1)
     scmd.addCommand("!VM", cmd_set_voltage_max);        // _ #Vmax (in range 0 to 3000)
+    scmd.addCommand("!LT", cmd_set_update_leds_time);   // _ #update_vsense_leds_time ()
 
 	// General:
+	scmd.addCommand("!PS", cmd_print_state);            // _ /  // Print state of the configuration
 	scmd.addCommand("!PA", cmd_setPrintAll);            // _ #printAll (0 or 1)
 	scmd.addCommand("Test", cmd_test);                  // _ (#a #b #c #d ...)
 	scmd.addDefaultHandler(unrecognized);
@@ -125,15 +146,20 @@ void loop() {
 	scmd.readSerial();
 
 	if (autoTimeoutOn) {
-		if (millis() > autoTimeoutTimer_1 + autoTimeoutTime) {
-			set_voltage_pin(0);
-		}
-
-        if (millis() > autoTimeoutTimer_2 + autoTimeoutTime) {
-			// TODO: set voltage of tip 2
-            // set_voltage_pin(0);
-		}
+        // Auto timeout for setting voltages to zero:
+        for (int i = 0; i <= 1; i++) {
+            if (millis() > auto_time_out_timers[i] + autoTimeoutTime) {
+                set_voltage_pin(i, 0);
+            }
+        }
 	}
+
+    for (int i = 0; i <= 1; i++) {
+        // Update leds to the values of Vsense pins if needed:
+        if (millis() > update_vsense_leds_time + update_vsense_leds_timers[i]) {
+            get_voltage_pin(i);
+        }
+    }
 	
 }
 
@@ -166,39 +192,47 @@ int16_t read_adc(int pin_number) {
 
 
 long AD_2_mV(long AD) {
-    // TODO: make the led for reading a bit nicer, and maybe read every 100 ms or so, not just when read is called.
-    leds[T1_VS_LED_NUMBER] = CRGB(0, 255*AD/32768, 0);  // green - Brightness as percent of "Vcc"
-    FastLED.show();
 	return AD*4710/32768; // 4700 mV is 32768 analog read value
 }
 
 
-void set_voltage_pin(int voltage) {
+void set_voltage_pin(int tip_number, int voltage) {
 	int DA;    // value to set to DAC
-    long computation_var;
     long long_type_one = 1;
 
 	// TODO:
 	// Do it nicer: Limit the output voltage yo Vmax (3V - or whatever)
     if (voltage > Vmax) {
         voltage = Vmax;
+        if (printAll) Serial.print(F("Voltage limit applied. Limit: ")); Serial.println(Vmax);
     }
 
-
+	// Set DAC:
 	DA = mV_2_DA(voltage);
-
-
-	// analogWrite(VinPin, DA);    
-	// analogWrite(VLEDPin, DA);
-    mcp4728.setChannelValue(T1_VIN_PIN_NUMBER, DA);
+    mcp4728.setChannelValue(vin_pin_of_tip[tip_number], DA);
+    if (printAll) Serial.print(F("Voltage set. Tip: ")); Serial.print(tip_number); Serial.print(F(", voltage: ")); Serial.println(voltage);
     
     // Set led:
-    // TODO: make the 5200 mV down here as a parameter somewhere on top.
-    computation_var = long_type_one*DA*255;
-    leds[T1_VIN_LED_NUMBER] = CRGB(0, computation_var/4095, 0);  // green - Brightness as percent of "Vcc"
+    leds[vin_led_of_tip[tip_number]] = CRGB(0, long_type_one*255*voltage/VCC, 0);  // green - Brightness as percent of "Vcc"
     FastLED.show();
 
-	autoTimeoutTimer_1 = millis();   
+	auto_time_out_timers[tip_number] = millis();   
+}
+
+long get_voltage_pin(int tip_number) {
+
+    long voltage = AD_2_mV(read_adc(vs_pin_of_tip[tip_number]));
+
+    // TODO: make the led for reading a bit nicer, and maybe read every 100 ms or so, not just when read is called.
+    leds[vs_led_of_tip[tip_number]] = CRGB(0, 255*voltage/VCC, 0);  // green - Brightness as percent of "Vcc"
+    FastLED.show();
+    update_vsense_leds_timers[tip_number] = millis();
+
+    return voltage;
+}
+
+bool is_valid_tip_number(int tip_number) {
+    return (0 <= tip_number && tip_number <= 1);
 }
 
 
@@ -274,7 +308,7 @@ void cmd_set_voltage() {
 	if ( arg != NULL) {
 		val = atol( arg );
 
-		set_voltage_pin(val);
+		set_voltage_pin(0, val);
 
     Serial.print(F("#VI ")); Serial.println(val);
 		
@@ -283,6 +317,61 @@ void cmd_set_voltage() {
 			Serial.println(val);
 		}
 	}
+}
+
+void cmd_set_single_tip() {
+    // Example call: !ST 0 500
+    char * arg;
+	int tip_number;
+	int voltage;
+
+	arg = scmd.next();
+	if (arg != NULL) {
+		tip_number = atol( arg );
+
+        arg = scmd.next();
+        if (arg != NULL && is_valid_tip_number(tip_number)) {
+    		voltage = atol( arg );
+
+		    set_voltage_pin(tip_number, voltage);
+
+            Serial.print(F("#ST ")); Serial.print(tip_number); Serial.print(F(" ")); Serial.println(voltage);
+            if (printAll) {
+                Serial.print(F("That was Vin set for tip "));
+                Serial.println(tip_number);
+            }
+        }
+	}
+}
+
+void cmd_set_all_tips() {
+    // Example call: !AT 150 400
+}
+
+void cmd_get_single_tip() {
+    // Example call: ?ST 0
+
+    char * arg;
+	int tip_number;
+
+	arg = scmd.next();
+	if (arg != NULL && is_valid_tip_number(tip_number)) {
+		tip_number = atol( arg );
+
+        Serial.print(F("#ST ")); Serial.println(get_voltage_pin(tip_number));
+        if (printAll) {
+            Serial.print(F("That was Vsense read for tip "));
+            Serial.println(tip_number);
+
+        }
+
+
+    }
+    
+}
+
+void cmd_get_all_tips() {
+    // Example call: ?AT
 }
 
 
@@ -297,6 +386,8 @@ void cmd_get_voltage () {
 	
 }
 
+// -------------------------------------------------------------------------------------
+// -- Commands for configuration: ------------------------------------------------------
 
 void cmd_set_auto_timeout_time () {
 	char * arg;
@@ -313,7 +404,6 @@ void cmd_set_auto_timeout_time () {
 	}
 }
 
-
 void cmd_set_auto_timeout_on () {
 	char * arg;
 	int val;
@@ -328,7 +418,6 @@ void cmd_set_auto_timeout_on () {
 		}
 	}
 }
-
 
 void cmd_set_voltage_max () {
 	char * arg;
@@ -352,7 +441,6 @@ void cmd_set_voltage_max () {
 	}
 }
 
-
 void cmd_setPrintAll() {
 	char * arg;
 	int val;
@@ -366,4 +454,32 @@ void cmd_setPrintAll() {
 			Serial.println(printAll);
 		}
 	}
+}
+
+void cmd_set_update_leds_time() {
+	char * arg;
+	int val;
+
+	arg = scmd.next();
+	if ( arg != NULL) {
+		val = atol( arg );
+		update_vsense_leds_time = val;
+		if (printAll) {
+			Serial.print(F("update_vsense_leds_time set to "));
+			Serial.println(update_vsense_leds_time);
+		}
+	}
+}
+
+void cmd_print_state() {
+    Serial.println(F("Printing configuration state:"));
+    Serial.print(F("  autoTimeoutOn: ")); Serial.println(autoTimeoutOn);
+    Serial.print(F("  autoTimeoutTime: ")); Serial.println(autoTimeoutTime);
+    Serial.print(F("  update_vsense_leds_time: ")); Serial.println(update_vsense_leds_time);
+    Serial.print(F("  Vmax: ")); Serial.println(Vmax);
+    Serial.print(F("  printAll: ")); Serial.println(printAll);
+    Serial.print(F("   - - ")); Serial.println();
+    Serial.print(F("  VCC: ")); Serial.println(VCC);
+    Serial.print(F("  N_BITS_ADC: ")); Serial.println(N_BITS_ADC);
+    Serial.print(F("  N_BITS_DAC: ")); Serial.println(N_BITS_DAC);
 }
